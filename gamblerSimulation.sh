50f27e87e76a14f15cc68284414e47fbe127d6a1
Rohan2596 <rohankadam662@gmail.com>
2019-11-14 12:17:32 +0530

    Checking for Win or loss

--- gamblerSimulation.sh
+++ gamblerSimulation.sh
@@ -2,10 +2,19 @@
 
 #Author ROHAN RAVINDRA KADAM
 #Date 14 NOV 2019
-#Purpose:- Gambling Simulation
-
 
 echo "Welcome to Gambler Simulation"
-GAMBLERSTAKE=100;
-GAMBLERBET=1;
+GAMBLER_STAKE=100;
+GAMBLER_BET=1;
 
+function bet(){
+   check=$(( RANDOM%2 ))
+	if [[ $check -eq 1  ]]
+	then
+		return 1;
+	else
+		return 0;
+	fi
+}
+
+bet
