bb69a39542c4774dd805b23ad29246b64d69b15b
Rohan2596 <rohankadam662@gmail.com>
2019-11-14 18:37:45 +0530

    Calculating Total Days or loss along with amount

--- gamblerSimulation.sh
+++ gamblerSimulation.sh
@@ -6,15 +6,24 @@
 echo "Welcome to Gambler Simulation"
 GAMBLER_STAKE=100;
 GAMBLER_BET=1;
+totalMonth=1;
 totalDaysInMonth=20;
 totalAmount=0;
 percentage=50;
+won=0
+loss=0
+finalWiningAmount=0
+finalLossAmount=0
+Final=0
 winLimit=$(( $GAMBLER_STAKE + $(( $GAMBLER_STAKE * $percentage / 100  )) ))
 loseLimit=$(( $GAMBLER_STAKE - $(( $GAMBLER_STAKE * $percentage / 100  )) ));
 
 declare -A betChart
 function bet(){
-for (( days=0; days<$totalDaysInMonth; days++ ))
+for (( month=0;month<=$totalMonth;month++  ))
+do
+
+for (( days=0; days<=$totalDaysInMonth; days++ ))
 do
 	dayStake=$GAMBLER_STAKE
 	while [ $dayStake -lt $winLimit ] && [ $dayStake -gt $loseLimit ]
@@ -23,17 +32,37 @@ do
 		if [[ $check -eq 1  ]]
 		then
 			dayStake=$(( $dayStake + $GAMBLER_BET));
+
 		else
 			dayStake=$(( $dayStake - $GAMBLER_BET));
-
 		fi
 	done
 echo $dayStake
-dailyAmount=$(( $dayStake - $GAMBLER_STAKE ))
+dailyAmount=$(( $dayStake ))
 totalAmount=$(( $totalAmount + $dailyAmount ))
 betChart["Days"$days]=$dayStake
 done
+
+done
+count
+ echo  ${betChart[@]}
+ echo	${!betChart[@]}
+ }
+function count(){
+
+for data in "${!betChart[@]}"
+do
+
+if [[ ${betChart[$data]} -eq 50  ]]
+then
+	won=$(( $won + 1 ))
+	finalWiningAmount=$(( $finalWiningAmount + 50 ))
+else
+	loss=$(( $loss + 1  ))
+	finalLossAmount=$(( $finalLossAmount - 50 ))
+fi
+final=$(( $finalWiningAmount + $finalLossAmount ))
+done
+
 }
 bet
-
-
