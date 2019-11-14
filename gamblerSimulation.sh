988caa1401a11b89acdef7abd00cb19040a11a78
Rohan2596 <rohankadam662@gmail.com>
2019-11-14 14:52:20 +0530

    Creating bet function

--- gamblerSimulation.sh
+++ gamblerSimulation.sh
@@ -1,5 +1,21 @@
+
 #!/bin/bash -x
 
 #Author ROHAN RAVINDRA KADAM
 #Date 14 NOV 2019
 
+echo "Welcome to Gambler Simulation"
+GAMBLER_STAKE=100;
+GAMBLER_BET=1;
+winLimit=1
+function bet(){
+   check=$(( RANDOM % 2 ))
+	if [[ $check -eq 1  ]]
+	then
+		return 1;
+	else
+		return 0;
+	fi
+}
+
+bet
